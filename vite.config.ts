5a5888989cdbd8619b60281dcd36f65a0a60f231
bcushing97 <bcush5797@gmail.com>
2025-11-30 19:05:01 -0500

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/travel-app/",
+  base: "/foundation-project/",
   resolve: {
     alias: [
       // "@/..." -> "<root>/src/..."
